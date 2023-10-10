da319b590a2551c8a147c0ea1172fab8b9f45eea
happer64bit <happer64bit@gmail.com>
2023-10-10 15:33:12 +0630

    IDK

--- app/api/getInfo/route.ts
+++ app/api/getInfo/route.ts
@@ -1,9 +1,15 @@
 import { NextRequest } from "next/server";
 import { TiktokDL } from '@tobyg74/tiktok-api-dl/lib/index';
 
-export async function POST(request: NextRequest) {
+export const runtime = "nodejs"
+
+export async function POST(request: NextRequest){
     const { URL } = await request.json()
 
+    request.headers.set("Access-Control-Allow-Origin", "*");
+    request.headers.set("Access-Control-Allow-Methods", "GET,POST,HEAD");
+    request.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  
     if(URL) {
         const result = await TiktokDL(URL).catch((err) => console.error(err))
 
